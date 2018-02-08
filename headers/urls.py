db8a70272fd67118dc53b36e014492c890389a4b
AwesomeBaron <urchmawni@gmail.com>
2018-02-08 21:42:18 +0100

    Update urls.py

--- headers/urls.py
+++ headers/urls.py
@@ -6,4 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
+    path('products/', views.products, name='products'),
+    path('blog/', views.blog, name='blog'),
 ]
